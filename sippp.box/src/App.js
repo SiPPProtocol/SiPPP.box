08239ef9b98059734387a4d6432fa76168b5e8f4
Valerie R. Coffman <valerie.coffman@gmail.com>
2024-06-30 19:12:22 -0700

    container fr

--- sippp.box/src/App.js
+++ sippp.box/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="container">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Sippp, or it didn't happen.
