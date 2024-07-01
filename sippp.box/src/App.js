b30adb82b5206acb4ae0b2f5ec938189d8915e63
Valerie R. Coffman <valerie.coffman@gmail.com>
2024-06-30 19:45:28 -0700

    try an id

--- sippp.box/src/App.js
+++ sippp.box/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="sipp">
-      <div className="sipp-container">
+    <div id="sipp">
+      <div className="container">
         <img src="https://www.sippp.box/sippp-logo-transparent.png" className="logo" alt="logo" />
         <p>
           Sippp, or it didn't happen.
