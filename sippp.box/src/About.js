a6938cc2ea8641da54c51f8fa12ffb3d8b578e9f
Valerie R. Coffman <valerie.coffman@gmail.com>
2024-06-30 20:29:28 -0700

    style about page

--- sippp.box/src/About.js
+++ sippp.box/src/About.js
@@ -8,12 +8,14 @@ function About() {
   }, []);
 
   return (
-    <div className="about-container">
-      <Link to="/">
-        <img src="https://www.sippp.box/sippp-logo-transparent.png" className="about-logo" alt="logo" />
-      </Link>
-      <h1>SiPPP: the Simple Photo Provenance Protocol</h1>
-      <p>This is the About page with some placeholder content. Here, you can provide information about your application or organization.</p>
+    <div id="sippp">
+        <div className="container">
+        <Link to="/">
+            <img src="https://www.sippp.box/sippp-logo-transparent.png" className="about-logo" alt="logo" />
+        </Link>
+        <h1>SiPPP: the Simple Photo Provenance Protocol</h1>
+        <p>This is the About page with some placeholder content. Here, you can provide information about your application or organization.</p>
+        </div>
     </div>
   );
 }
