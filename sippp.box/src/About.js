6ef5e86dc880c895e865adf9c7e1672085e4f79b
Valerie R. Coffman <valerie.coffman@gmail.com>
2024-06-30 20:38:17 -0700

    tweaking typography more

--- sippp.box/src/About.js
+++ sippp.box/src/About.js
@@ -14,7 +14,15 @@ function About() {
             <img src="https://www.sippp.box/sippp-logo-transparent.png" className="about-logo" alt="logo" />
         </Link>
         <h1>SiPPP: the Simple Photo Provenance Protocol</h1>
+
+        <h2>What</h2>
         <p>This is the About page with some placeholder content. Here, you can provide information about your application or organization.</p>
+
+        <h2>Why</h2>
+        <p>tktktk</p>
+
+        <h2>How</h2>
+        <p>tktktk</p>
         </div>
     </div>
   );
